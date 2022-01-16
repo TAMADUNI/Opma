7be6411062089c16b71636825c6b17e773331f81
IBUNHABIBU <ibunhabib@gmail.com>
2022-01-17 00:12:47 +0300

    Configure factory bot

--- attendance/spec/factories/user.rb
+++ attendance/spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
- factory(:user) do
-   email { "jane.doe@hey.com" }
-   password { "SecretPassword123" }
- end
+  factory(:user) do
+    email { 'jane.doe@hey.com' }
+    password { 'SecretPassword123' }
+  end
 end
