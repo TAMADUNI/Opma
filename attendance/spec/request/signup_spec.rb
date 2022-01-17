274250f8ecaed9c06a685368e79157538b22765f
IBUNHABIBU <ibunhabib@gmail.com>
2022-01-17 23:20:09 +0300

    Create request spec

--- attendance/spec/request/signup_spec.rb
+++ attendance/spec/request/signup_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 describe 'User signup', type: :request do
   let(:email) { build(:email) }
   let(:password) { build(:password) }
+
+  before do
+    visit new_registration_path
+  end
+
+  scenario 'With valid data' do
+    fill_in 'user_email', with: email
+    fill_in 'password', with: password
+    click_button 'Register'
+    expect(page).to have_content('You have registered successfully.')
+  end
 end
