b4277f3a7ff0298329ba6674df2640b1c954e878
Ibunhabibu <Ibunhabib@gmail.com>
2023-10-28 20:43:47 +0300

    Rearanging the folders

--- attendance/app/views/attends/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @attends, partial: "attends/attend", as: :attend
--- attendance/db/migrate/20220912120101_create_attends.rb
+++
@@ -1,8 +0,0 @@
-class CreateAttends < ActiveRecord::Migration[7.0]
-  def change
-    create_table :attends do |t|
-
-      t.timestamps
-    end
-  end
-end
