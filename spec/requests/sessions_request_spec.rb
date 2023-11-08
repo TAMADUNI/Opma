b0ee97898f7bf79d8c8f8fedf003dd46e6f3541f
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-08 08:17:07 +0300

    Add session spec

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,2 +1,8 @@
-RSpec.describe "User sessions ", type: :request do 
+RSpec.describe "User sessions ", type: :request do
+    describe "POST /users/sign_in" do
+        let(:user) { create(:user, email: "test@example.com", password: "123")}
+
+        it 'Signs in with valid email and password' do 
+        end
+    end
 end
