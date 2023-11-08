dce87fd383c37c5878102a2e81784fe0eb3a24a3
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-08 08:47:30 +0300

    Add feature spec for authentication

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -9,4 +9,22 @@ describe 'User authentication' do
         click_on 'Log in'
         expect(page).to have_content("Signed in successfully.")
     end
+
+    scenario 'User cannot sign in with invalid password' do
+        user = create(:user, email: "test@example.com", password: "123")
+        visit new_user_session_path
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: "1234"
+        click_on 'Log in'
+        expect(page).to have_content("Invalid Email or password.")
+    end
+
+    scenario 'User cannot sign in with invalid email' do
+        user = create(:user, email: "text@example.com", password: "123")
+        visit new_user_session_path
+        fill_in 'Email', with: "jumaa@juma.com"
+        fill_in 'Password', with: user.password
+        click_on 'Log in'
+        expect(page).to have_content("Invalid Email or password.")
+    end
 end
