3aa9cd65c71442780530ea7de61f1da65ab45892
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-08 08:48:13 +0300

    Add feature spec for authentication

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -24,7 +24,7 @@ describe 'User authentication' do
         visit new_user_session_path
         fill_in 'Email', with: "jumaa@juma.com"
         fill_in 'Password', with: user.password
-        click_on 'Log in'
+        click_button 'Log in'
         expect(page).to have_content("Invalid Email or password.")
     end
 end
