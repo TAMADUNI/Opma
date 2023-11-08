b8445cee3fbc23f869981933e8b3a3c9a9015bff
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-08 08:45:13 +0300

    Add feature spec for authentication

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe 'User authentication' do
-    it 'Allows a user to sign up' do 
+    scenario 'User can sign in with valid email and password' do
+        user = create(:user, email: "test@example.com", password: "123")
+        visit new_user_session_path
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+        click_on 'Log in'
+        expect(page).to have_content("Signed in successfully.")
     end
 end
