c20db244162c7d5c2bb8024f419714b2c58ba13f
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-08 08:30:13 +0300

    Add session spec

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,7 +2,11 @@ RSpec.describe "User sessions ", type: :request do
     describe "POST /users/sign_in" do
         let(:user) { create(:user, email: "test@example.com", password: "123")}
 
-        it 'Signs in with valid email and password' do 
+        it 'Signs in with valid email and password' do
+            post user_session_path, params: { user: { email: user.email, password: user.password } } 
+            expect(response).to redirect_to(root_path)
+            follow_redirect!
+            expect(response.body).to include("Signed in successfully.")
         end
     end
 end
