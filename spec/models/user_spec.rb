a32164b811859eca5a3d2b3963f0e4e0ed3ea878
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-10 18:19:17 +0300

    Test user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # let(:user) { build(:user) }
   it 'is valid with valid attributes ' do
-    user = create(:user)
+    user = build(:user)
     expect(user).to be_valid
   end
 
