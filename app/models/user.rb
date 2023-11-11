c3d713793f777b8b62734994d30aba076335787e
Ibunhabibu <Ibunhabib@gmail.com>
2023-11-11 21:00:30 +0300

    Finish user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { in: 8..20  },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: true }, on: :create
+  validates :password, presence: true, length: { in: 3..20  }, on: :create
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  validates :email, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,17 +5,11 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    it { should validate_uniqueness_of(:email) }
-    it { should validate_length_of(:password).is_at_least(3) }
-    it { should validate_length_of(:password).is_at_most(20) }
-    it { should validate_length_of(:email).is_at_most(20) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_length_of(:password).is_at_least(3).on(:create) }
+    it { should validate_length_of(:password).is_at_most(20).on(:create) }
+    it { should validate_length_of(:email).is_at_most(20).on(:create) }
     it { should validate_length_of(:email).is_at_least(8).on(:create) }
   end
-  
-  # describe 'Factory' do
-  #   it 'should have valid Factory' do
-  #     expect(create(:user)).to be_valid
-  #   end
-  # end
- 
+
 end
