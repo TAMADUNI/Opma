60cb4e6e470b84a2d9ca583c72425ebec242ef63
Ibunhabibu <Ibunhabib@gmail.com>
2024-01-14 22:46:57 +0300

    Fix error in the line.rb

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,6 +1,6 @@
 class Line < ApplicationRecord
   enum status: [:running, :off_production]
-  enum maintenance_type [:regular, :planned]
+  enum maintenance_type: [:regular, :planned]
   belongs_to :site
   has_and_belongs_to_many :tasks
 end
